443f4a51b094ba24f6556fa017bdaf090a5eb1e9
Tayyab-Ejaz <tayyabejaz668@gmail.com>
2022-07-25 16:52:17 +0500

    Refactoring

--- app/controllers/flight_paths_controller.rb
+++ app/controllers/flight_paths_controller.rb
@@ -1,15 +1,10 @@
 class FlightPathsController < ApplicationController
-  before_action :flight_paths, only: [:index]
-
   def index
+    @flight_paths = FlightPath.all
   end
 
   def calculate_flight
     @flight_path = DistanceApi.new(flight_params).get_flight_path
-  end
-
-  def flight_paths
-    @flight_paths = FlightPath.all
   end
 
   def flight_params
